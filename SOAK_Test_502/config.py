901f62e3ed0716a349be4ee85329bb4a8cd9d144
hsaikia555 <hsaikia555@gmail.com>
2018-10-05 16:14:34 +0530

    Adding Project SOAK Test-502

---
+++ SOAK_Test_502/config.py
@@ -0,0 +1,14 @@
+PROTOCOL = "http"
+HOST = "10.11.13.54"
+PORT = "8080"
+USERNAME = "admin"
+PASSWORD = "admin"
+project = "1"
+WF_BASE_PATH = "/user/zaloni/"
+LOCAL_BASE_DIRECTORY = "/home/zaloni/"
+FILE_SYSTEM_URI = "hdfs://mica-perf-n1.zalonilabs.com:8020"
+FILE_SYSTEM_ID = "2"
+#For Local=1,HDFS=2,S3=3,Azure=4
+
+
+
